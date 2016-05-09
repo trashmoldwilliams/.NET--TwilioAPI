5d69294d85ffbe95662129bf3b2d7910e34c9bc2
Will Johnson <trashmoldwilliams@gmail.com>
2016-05-09 09:26:23 -0700

    Build test.

---
+++ src/Texter/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Mvc;
+using Texter.Models;
+
+namespace Texter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetMessages()
+        {
+            var allMessages = Message.GetMessages();
+            return View(allMessages);
+        }
+
+        public IActionResult SendMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage(Message newMessage)
+        {
+            newMessage.Send();
+            return RedirectToAction("Index");
+        }
+    }
+}
